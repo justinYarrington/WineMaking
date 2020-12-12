f53a72b8ab804b954930475772de01fee739afe9
justinYarrington <46681472+justinYarrington@users.noreply.github.com>
2020-12-11 19:51:21 -0600

    changing logic so we don't have to wait an arbitray amount of time between each load of wines. Also we are just pressing space for making wines instead of finding a picture.

--- autoWiner.py
+++ autoWiner.py
@@ -67,42 +67,44 @@ if __name__ == '__main__':
                 inventoryImage = pag.locateOnScreen('images\\inventoryClosed.png', confidence=0.95)
                 clickIcon(inventoryImage)
             
-            """Begin process of depositing good/bad wines"""
-            # Find banker and click it
-            banker = pag.locateOnScreen('images\\bankWindow.png', confidence=0.55)
-            clickIcon(banker)
+            if (pag.locateOnScreen('images\\berryInInventory.png', confidence=0.9)):
+                pass
+            else:
+                """Begin process of depositing good/bad wines"""
+                # Find banker and click it
+                banker = pag.locateOnScreen('images\\bankWindow.png', confidence=0.55)
+                clickIcon(banker)
 
-            # Deposit fermenting wines. 
-            deposit = pag.locateOnScreen('deposit\\depositThis.png', confidence=0.9)
-            clickIcon(deposit)
-            random_wait(0.5, 1)
-            deposit = pag.locateOnScreen('deposit\\depositThis.png', confidence=0.9)
-            clickIcon(deposit)
+                # Deposit fermenting wines. 
+                deposit = pag.locateOnScreen('deposit\\depositThis.png', confidence=0.9)
+                clickIcon(deposit)
+                random_wait(0.5, 1)
+                deposit = pag.locateOnScreen('deposit\\depositThis.png', confidence=0.9)
+                clickIcon(deposit)
 
-            # Withdraw 14 jugs of water and 14 berries. 
-            withdraw14jugs = pag.locateOnScreen('images\\jugOfWater.png', confidence=0.95)
-            withdraw14berries = pag.locateOnScreen('images\\berries.png', confidence=0.9)
-            rightclickIcon(withdraw14jugs)
-            withdraw14 = pag.locateOnScreen('images\\withdraw14.png', confidence=0.95)
-            clickIcon(withdraw14)
-            rightclickIcon(withdraw14berries)
-            withdraw14 = pag.locateOnScreen('images\\withdraw14.png', confidence=0.95)
-            clickIcon(withdraw14)
-            
+                # Withdraw 14 jugs of water and 14 berries. 
+                withdraw14jugs = pag.locateOnScreen('images\\jugOfWater.png', confidence=0.95)
+                withdraw14berries = pag.locateOnScreen('images\\berries.png', confidence=0.9)
+                rightclickIcon(withdraw14jugs)
+                withdraw14 = pag.locateOnScreen('images\\withdraw14.png', confidence=0.95)
+                clickIcon(withdraw14)
+                rightclickIcon(withdraw14berries)
+                withdraw14 = pag.locateOnScreen('images\\withdraw14.png', confidence=0.95)
+                clickIcon(withdraw14)
+                
 
-            # close out of the bank
-            closeBank = pag.locateOnScreen('images\\closeBank.png', confidence=0.9)
-            clickIcon(closeBank)
+                # close out of the bank
+                closeBank = pag.locateOnScreen('images\\closeBank.png', confidence=0.9)
+                clickIcon(closeBank)
 
-            #begin process of making wines
-            clickJugOfWater = pag.locateOnScreen('images\\waterInInventory.png', confidence=0.9)
-            clickBerries = pag.locateOnScreen('images\\berryInInventory.png', confidence=0.9)
-            clickIcon(clickJugOfWater)
-            clickIcon(clickBerries)
-            random_wait(1, 2)
-            makeWines = pag.locateOnScreen('images\\clickWineJug.png', confidence=0.9)
-            clickIcon(makeWines)
-            random_wait(17, 20)
+                #begin process of making wines
+                clickJugOfWater = pag.locateOnScreen('images\\waterInInventory.png', confidence=0.9)
+                clickBerries = pag.locateOnScreen('images\\berryInInventory.png', confidence=0.9)
+                clickIcon(clickJugOfWater)
+                clickIcon(clickBerries)
+                random_wait(1, 2)
+                pag.press('space')
+                random_wait(17, 20)
 
     except KeyboardInterrupt:
         sys.exit()
